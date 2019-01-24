f3beab24614d9d4de16c0a186cae198e75429133
MaynardMiner <MaynardMiner>
2019-01-24 17:58:49 -0500

    cuda 9.2 note

--- configure.sh
+++ configure.sh
@@ -7,4 +7,4 @@ extracflags="-march=native -D_REENTRANT -falign-functions=16 -falign-jumps=16 -f
 
 CUDA_CFLAGS="-O3 -lineno -Xcompiler -Wall  -D_FORCE_INLINES" \
 	./configure CXXFLAGS="-O3 $extracflags" --with-cuda=/usr/local/cuda-10.0 --with-nvml=libnvidia-ml.so
-											##--with-cuda=/usr/local/cuda is an alternative
+											##--with-cuda=/usr/local/cuda is an alternative (Cuda 9.2)
